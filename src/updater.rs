d0b6f411023b1297708f100263924500cc4f5640
Alexander Tuktarov <atuktarov@web3tech.ru>
2021-06-23 10:43:55 +0000

    Release/v1.3.0

--- src/updater.rs
+++ src/updater.rs
@@ -1,6 +1,7 @@
-use crate::{error::Error, models::Topic};
+use crate::error::Error;
 use std::convert::TryFrom;
-use wavesexchange_log::error;
+use wavesexchange_log::info;
+use wavesexchange_topic::Topic;
 
 // NB: redis server has to be configured to publish keyspace notifications:
 // https://redis.io/topics/notifications
@@ -8,11 +9,11 @@ pub fn run(
     redis_client: redis::Client,
     updates_sender: tokio::sync::mpsc::UnboundedSender<Topic>,
 ) -> Result<(), Error> {
-    'base: loop {
+    loop {
         let mut conn = redis_client.get_connection()?;
         let mut pubsub = conn.as_pubsub();
-        pubsub.set_read_timeout(Some(std::time::Duration::from_secs(240)))?;
-        pubsub.psubscribe("__keyevent*__:*")?;
+        pubsub.set_read_timeout(Some(std::time::Duration::from_secs(60)))?;
+        pubsub.psubscribe("__keyevent*__:set")?;
         loop {
             match pubsub.get_message() {
                 Ok(msg) => {
@@ -21,16 +22,16 @@ pub fn run(
                             if let Topic::Transaction(_) = topic {
                                 continue;
                             }
-                            if let Err(err) = updates_sender.send(topic) {
-                                error!("error occured while sending resource update: {:?}", err);
-                                break 'base;
-                            }
+                            updates_sender
+                                .send(topic)
+                                .expect("error occured while sending resource update")
                         }
                     }
                 }
                 Err(error) => {
                     // error when socket don't response in time
                     if error.to_string().contains("os error 11") {
+                        info!("updater don't get new events, reopen connection");
                         break;
                     }
                     return Err(error.into());
@@ -38,6 +39,4 @@ pub fn run(
             }
         }
     }
-
-    Ok(())
 }
